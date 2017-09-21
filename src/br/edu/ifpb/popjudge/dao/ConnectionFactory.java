2b0ee3c79b02756248d0db7363b70641cb51a5cd
Wemerson T. Vital Porto <wemerson.thayne@gmail.com>
2017-09-21 19:42:16 -0300

    heroku user

--- src/br/edu/ifpb/popjudge/dao/ConnectionFactory.java
+++ src/br/edu/ifpb/popjudge/dao/ConnectionFactory.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 	private static final String DRIVER = "com.mysql.jdbc.Driver";
-	private static final String URL = "jdbc:mysql://localhost:3306/POP_JUDGE?useSSL=false";
-	private static final String USER = "root";
-	private static final String PASS = "root";
+	private static final String URL = "jmysql://xq7t6tasopo9xxbs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/jc1q6saag9lc5q1d/POP_JUDGE?useSSL=false";
+	private static final String USER = "fy06tbg72m8rava4";
+	private static final String PASS = "cbyy8rrf033m81qn";
 	public Connection getConnection() throws SQLException{
 		
 //		try {
